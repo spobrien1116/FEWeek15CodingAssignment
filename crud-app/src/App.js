043b15ee22c2b1f9990a646aa6c7a62f1c445f5f
spobrien1116 <87784429+spobrien1116@users.noreply.github.com>
2022-09-06 20:33:26 -0400

    Set up initial project
    This is just the creation of the create-react-app. No structure or any other content besides the most minor of setup changes.

---
+++ crud-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <br></br>
+      <p className="creator">Created by: Shawn O'Brien</p>  
+    </div>
+  );
+}
+
+export default App;
